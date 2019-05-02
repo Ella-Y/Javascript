e06dc3dfcb957281d12c5470b399cd816bc68473
김예지 <soph456@naver.com>
2019-05-02 16:07:26 +0900

    initial commit

---
+++ javascript/project/lotto.js
@@ -0,0 +1,26 @@
+var ary=[];
+
+function randInt(min,max){ //random included min and max
+    return Math.floor(Math.random()*(max-min+1))+min;
+}
+
+function findLotto(){
+    for(var i=0;i<7;i++){
+        while(true){
+            var val=randInt(1,45);
+            if(ary.length==0 || ary.indexOf(val)==-1){
+                ary.push(val);
+                break;
+            }
+        }
+    }
+    
+}
+
+function printLotto(){
+    findLotto();
+    for(var i=0;i<7;i++){
+        document.getElementById('l'+(i+1).toString()).innerHTML=ary[i];
+    }
+}
+
